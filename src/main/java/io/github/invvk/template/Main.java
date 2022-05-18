e53c83baed537bb2f5b67744d154952cf14f2e0f
Derek <70810073+Invvk@users.noreply.github.com>
2022-05-18 05:05:11 -0700

    initial

---
+++ src/main/java/io/github/invvk/template/Main.java
@@ -0,0 +1,17 @@
+package io.github.invvk.template;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
